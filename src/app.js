ba7dd0b395fd195224ebb702ab216d6b6114d163
k-jamie <79115673+k-jamie@users.noreply.github.com>
2021-04-11 21:29:23 +0100

    added weather icons

--- src/app.js
+++ src/app.js
@@ -30,6 +30,7 @@ function displayTemperature(response) {
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
     let dateElement = document.querySelector("#date");
+    let iconElement = document.querySelector("#weather-icon");
 
     celsiusTemperature = response.data.main.temp;
 
@@ -40,7 +41,8 @@ function displayTemperature(response) {
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
     dateElement.innerHTML = formatDate(response.data.dt * 1000);
-    console.log(response.data);
+    iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@4x.png`);
+    iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 function search(city) {
